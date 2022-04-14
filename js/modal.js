51a4a2cc100a2c20b9b3247d39499831a57aeb3b
Ulises Montenegro <ulinicolasmonte1@gmail.com>
2022-04-14 10:47:43 -0300

    cambios eventos

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,13 @@
 
 const modalContenedor = document.querySelector('.modal-contenedor')
 const abrirCarrito = document.getElementById('carritoDeCompras');
+const cerrarCarrito = document.getElementById('botonCerrar');
 
 abrirCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active');
+});
+
+cerrarCarrito.addEventListener('click', () =>{
     modalContenedor.classList.toggle('modal-active');
 });
 
